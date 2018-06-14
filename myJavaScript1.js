5ceeba3a31e7a864418bc2bb5c8939482e2d784e
Isaac-001 <40184475+Isaac-001@users.noreply.github.com>
2018-06-13 18:56:19 -0700

    Update myJavaScript1.js

--- myJavaScript1.js
+++ myJavaScript1.js
@@ -1,3 +1,3 @@
-function myfunc(){document.getElementById("demo").innerHTML= "this dude eating beans!";}
+<big>function myfunc(){document.getElementById("demo").innerHTML= "this dude eating beans!";}</big>
 
 var today=new Date()
