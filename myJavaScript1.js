bc11c5e991e50b94a120e13beda05d76f5be61e1
Isaac-001 <40184475+Isaac-001@users.noreply.github.com>
2018-06-15 09:54:18 -0700

    Add files via upload

--- myJavaScript1.js
+++ myJavaScript1.js
@@ -1,5 +1,6 @@
 function myfunc(){document.getElementById("demo").innerHTML= "'This dude eating BEEEEANS!'";}
 function DAFUNC(){document.getElementById("demo").innerHTML= "'HE REALLY EATING BEANNNNNS'";}
+function oioi(){document.getElementById("demo").innerHTML= "*laughter*";}
 
 var today=new Date()
 
